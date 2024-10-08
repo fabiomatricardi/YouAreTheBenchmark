ce31428a81e505fce8c50c348bbf36d04a3e2288
Fabio Matricardi <fabio.matricardi@gmail.com>
2024-10-08 12:27:04 +0800

    Add files via upload

---
+++ 091.Qwen2.5-1.5B_LlamaCPP_promptTest.py
@@ -0,0 +1,158 @@
+# Chat with an intelligent assistant in your terminal  
+# MODEL: https://huggingface.co/Qwen
+# qwen2.5-1.5b-instruct-q5_k_m.gguf
+import sys
+from time import sleep
+import warnings
+warnings.filterwarnings(action='ignore')
+import datetime
+from promptLibv2Qwen import countTokens, writehistory, createCatalog
+from promptLibv2Qwen import genRANstring, createStats
+import argparse
+## PREPARING FINAL DATASET
+
+pd_id = []
+pd_task = []
+pd_vote = []
+pd_remarks = []
+####################Add GPU argument in the parser###################################
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--gpu", type=int, default=0,nargs='?',
+                    help="The number of layers to load on GPU")
+args = parser.parse_args()
+if args.gpu == None:
+   ngpu_layers = 0 
+else:
+    ngpu_layers = args.gpu
+print(f'Selected GPU: offloading {ngpu_layers} layers...')   
+####################INITIALIZE THE MODEL###################################
+stops = ['<|im_end|>']
+tasks = createCatalog()
+modelname = 'qwen2.5-1.5b-instruct-q5_k_m.gguf'
+# create THE LOG FILE 
+coded5 = genRANstring(5)
+logfile = f'logs/Qwen2.5-1.5B-it_CPP_{coded5}_log.txt'
+csvfile = f'logs/Qwen2.5-1.5B-it_CPP_{coded5}.csv'
+logfilename = logfile
+#Write in the history the first 2 sessions
+writehistory(logfilename,f'{str(datetime.datetime.now())}\n\nYour own LocalGPT with 💻 {modelname}\n---\n🧠🫡: You are a helpful assistant.')    
+writehistory(logfilename,f'💻: How can I assist you today in writing?')
+# LOAD THE MODEL
+print("\033[95;3;6m")
+print("1. Waiting 10 seconds for the API to load...")
+from llama_cpp import Llama
+llm = Llama(
+            model_path='models/qwen2.5-1.5b-instruct-q5_k_m.gguf',
+            n_gpu_layers=ngpu_layers,
+            temperature=0.1,
+            n_ctx=8192,
+            max_tokens=1500,
+            repeat_penalty=1.178,
+            stop=stops,
+            verbose=False,
+            )
+print(f"2. Model {modelname} loaded with LlamaCPP...")
+print("\033[0m")  #reset all
+history = []
+print("\033[92;1m")
+print(f'📝Logfile: {logfilename}')
+##################### ALIGNMENT FIRST GENERATION ##############################################
+question = 'Explain the plot of Cinderella in a sentence.'
+test = [
+    {"role": "user", "content": question}
+]
+print('Question:', question)
+start = datetime.datetime.now()
+print("💻 > ", end="", flush=True)
+full_response = ""
+fisrtround = 0
+for chunk in llm.create_chat_completion(
+    messages=test,
+    temperature=0.25,
+    repeat_penalty= 1.31,
+    stop=stops,
+    max_tokens=1500,
+    stream=True,):
+    try:
+        if chunk["choices"][0]["delta"]["content"]:
+            if fisrtround==0:
+                print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                full_response += chunk["choices"][0]["delta"]["content"]
+                ttftoken = datetime.datetime.now() - start  
+                fisrtround = 1
+            else:
+                print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                full_response += chunk["choices"][0]["delta"]["content"]                              
+    except:
+        pass        
+delta = datetime.datetime.now() - start
+output = full_response
+print('')
+print("\033[91;1m")
+rating = input('Rate from 0 (BAD) to 5 (VERY GOOD) the quality of generation> ')
+print("\033[92;1m")
+stats = createStats(delta,question,output,rating,logfilename,'Alignment Generation',ttftoken)
+print(stats)
+writehistory(logfilename,f'''👨‍💻 . {question}
+💻 > {output}
+{stats}
+''')
+
+############################# AUTOMATIC PROMPTING EVALUATION  11 TURNS #################################
+id =1
+for items in tasks:
+    fisrtround = 0
+    task = items["task"]
+    prompt = items["prompt"]
+    test = []
+    print(f'NLP TAKS>>> {task}')
+    print("\033[91;1m")  #red
+    print(prompt)
+    test.append({"role": "user", "content": prompt})
+    print("\033[92;1m")
+    full_response = ""
+    start = datetime.datetime.now()
+    print("💻 > ", end="", flush=True)
+    for chunk in llm.create_chat_completion(
+        messages=test,
+        temperature=0.15,
+        repeat_penalty= 1.31,
+        stop=stops,
+        max_tokens=1500,
+        stream=True,):
+        try:
+            if chunk["choices"][0]["delta"]["content"]:
+                if fisrtround==0:
+                    print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                    full_response += chunk["choices"][0]["delta"]["content"]
+                    ttftoken = datetime.datetime.now() - start  
+                    fisrtround = 1
+                else:
+                    print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                    full_response += chunk["choices"][0]["delta"]["content"]                              
+        except:
+            pass        
+    delta = datetime.datetime.now() - start
+    print('')
+    print("\033[91;1m")
+    rating = input('Rate from 0 (BAD) to 5 (VERY GOOD) the quality of generation> ')
+    print("\033[92;1m")
+    stats = createStats(delta,prompt,full_response,rating,logfilename,task,ttftoken)
+    print(stats)
+    writehistory(logfilename,f'''👨‍💻 > {prompt}
+💻 > {full_response}
+{stats}
+''')
+    pd_id.append(id)
+    pd_task.append(task)
+    pd_vote.append(rating[:2])
+    pd_remarks.append(rating[2:])
+    id += 1
+# create dataframe and save to csv
+zipped = list(zip(pd_id,pd_task,pd_vote,pd_remarks))
+import pandas as pdd
+df = pdd.DataFrame(zipped, columns=['#', 'TASK', 'VOTE','REMARKS'])
+#saving the DataFrame as a CSV file 
+df_csv_data = df.to_csv(csvfile, index = False, encoding='utf-8') 
+print('\nCSV String:\n', df_csv_data)     
+
