57653acb13e1834fe2a5a0069351c903bbf4ed9a
Fabio Matricardi <fabio.matricardi@gmail.com>
2024-10-14 17:12:12 +0800

    Add files via upload

---
+++ ARCEE-LITE-1.5B/200.Arcee-Lite-1.5B_LlamaCPP_promptTest.py
@@ -0,0 +1,205 @@
+# Chat with an intelligent assistant in your terminal 
+# original https://huggingface.co/arcee-ai/arcee-lite 
+# MODEL: https://huggingface.co/arcee-ai/arcee-lite-GGUF
+# models/arcee-lite-Q5_K_M.gguf
+import sys
+from time import sleep
+import warnings
+warnings.filterwarnings(action='ignore')
+import datetime
+from promptLibv2Qwen import countTokens, writehistory, createCatalog
+from promptLibv2Qwen import genRANstring, createStats
+import argparse
+## PREPARING FINAL DATASET
+
+pd_id = []
+pd_task = []
+pd_vote = []
+pd_remarks = []
+####################Add GPU argument in the parser###################################
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--gpu", type=int, default=0,nargs='?',
+                    help="The number of layers to load on GPU")
+args = parser.parse_args()
+if args.gpu == None:
+   ngpu_layers = 0 
+else:
+    ngpu_layers = args.gpu
+print(f'Selected GPU: offloading {ngpu_layers} layers...')   
+####################INITIALIZE THE MODEL###################################
+stops = ['<|im_end|>']
+tasks = createCatalog()
+modelname = 'arcee-lite-Q5_K_M.gguf'
+# create THE LOG FILE 
+coded5 = genRANstring(5)
+logfile = f'logs/arcee-lite_CPP_{coded5}_log.txt'
+csvfile = f'logs/arcee-lite_CPP_{coded5}.csv'
+logfilename = logfile
+#Write in the history the first 2 sessions
+writehistory(logfilename,f'{str(datetime.datetime.now())}\n\nYour own LocalGPT with 💻 {modelname}\n---\n🧠🫡: You are a helpful assistant.')    
+writehistory(logfilename,f'💻: How can I assist you today in writing?')
+# LOAD THE MODEL
+print("\033[95;3;6m")
+print("1. Waiting 10 seconds for the API to load...")
+from llama_cpp import Llama
+llm = Llama(
+            model_path=f'models/{modelname}',
+            n_gpu_layers=ngpu_layers,
+            temperature=0.1,
+            n_ctx=8192,
+            max_tokens=1500,
+            repeat_penalty=1.178,
+            stop=stops,
+            verbose=False,
+            )
+print(f"2. Model {modelname} loaded with LlamaCPP...")
+print("\033[0m")  #reset all
+history = []
+print("\033[92;1m")
+print(f'📝Logfile: {logfilename}')
+##################### ALIGNMENT FIRST GENERATION ##############################################
+question = 'Explain the plot of Cinderella in a sentence.'
+test = [
+    {"role": "user", "content": question}
+]
+print('Question:', question)
+start = datetime.datetime.now()
+print("💻 > ", end="", flush=True)
+full_response = ""
+fisrtround = 0
+for chunk in llm.create_chat_completion(
+    messages=test,
+    temperature=0.25,
+    repeat_penalty= 1.31,
+    stop=stops,
+    max_tokens=1500,
+    stream=True,):
+    try:
+        if chunk["choices"][0]["delta"]["content"]:
+            if fisrtround==0:
+                print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                full_response += chunk["choices"][0]["delta"]["content"]
+                ttftoken = datetime.datetime.now() - start  
+                fisrtround = 1
+            else:
+                print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                full_response += chunk["choices"][0]["delta"]["content"]                              
+    except:
+        pass        
+delta = datetime.datetime.now() - start
+output = full_response
+print('')
+print("\033[91;1m")
+rating = input('Rate from 0 (BAD) to 5 (VERY GOOD) the quality of generation> ')
+print("\033[92;1m")
+stats = createStats(delta,question,output,rating,logfilename,'Alignment Generation',ttftoken)
+print(stats)
+writehistory(logfilename,f'''👨‍💻 . {question}
+💻 > {output}
+{stats}
+''')
+
+############################# AUTOMATIC PROMPTING EVALUATION  11 TURNS #################################
+id =1
+for items in tasks:
+    fisrtround = 0
+    task = items["task"]
+    prompt = items["prompt"]
+    test = []
+    print(f'NLP TAKS>>> {task}')
+    print("\033[91;1m")  #red
+    print(prompt)
+    test.append({"role": "user", "content": prompt})
+    print("\033[92;1m")
+    full_response = ""
+    start = datetime.datetime.now()
+    print("💻 > ", end="", flush=True)
+    for chunk in llm.create_chat_completion(
+        messages=test,
+        temperature=0.15,
+        repeat_penalty= 1.31,
+        stop=stops,
+        max_tokens=1500,
+        stream=True,):
+        try:
+            if chunk["choices"][0]["delta"]["content"]:
+                if fisrtround==0:
+                    print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                    full_response += chunk["choices"][0]["delta"]["content"]
+                    ttftoken = datetime.datetime.now() - start  
+                    fisrtround = 1
+                else:
+                    print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                    full_response += chunk["choices"][0]["delta"]["content"]                              
+        except:
+            pass        
+    delta = datetime.datetime.now() - start
+    print('')
+    print("\033[91;1m")
+    rating = input('Rate from 0 (BAD) to 5 (VERY GOOD) the quality of generation> ')
+    print("\033[92;1m")
+    stats = createStats(delta,prompt,full_response,rating,logfilename,task,ttftoken)
+    print(stats)
+    writehistory(logfilename,f'''👨‍💻 > {prompt}
+💻 > {full_response}
+{stats}
+''')
+    pd_id.append(id)
+    pd_task.append(task)
+    pd_vote.append(rating[:2])
+    pd_remarks.append(rating[2:])
+    id += 1
+# create dataframe and save to csv
+zipped = list(zip(pd_id,pd_task,pd_vote,pd_remarks))
+import pandas as pdd
+df = pdd.DataFrame(zipped, columns=['#', 'TASK', 'VOTE','REMARKS'])
+#saving the DataFrame as a CSV file 
+df_csv_data = df.to_csv(csvfile, index = False, encoding='utf-8') 
+print('\nCSV String:\n', df)     
+from rich.console import Console
+console = Console()
+console.print('---')
+console.print(df)
+
+##############################MODEL CARD###############################################
+"""
+
+Prompt format
+```
+<|im_start|>system
+{system_prompt}<|im_end|>
+<|im_start|>user
+{prompt}<|im_end|>
+<|im_start|>assistant
+```
+
+llm_load_print_meta: format           = GGUF V3 (latest)
+llm_load_print_meta: arch             = qwen2
+llm_load_print_meta: vocab type       = BPE
+llm_load_print_meta: n_vocab          = 151936
+llm_load_print_meta: n_merges         = 151387
+llm_load_print_meta: vocab_only       = 0
+llm_load_print_meta: n_ctx_train      = 32768
+llm_load_print_meta: n_embd           = 1536
+llm_load_print_meta: n_layer          = 28
+llm_load_print_meta: model ftype      = Q5_K - Medium
+llm_load_print_meta: model params     = 1.78 B
+llm_load_print_meta: model size       = 1.19 GiB (5.76 BPW)
+llm_load_print_meta: general.name     = Qwen2 1.5B
+llm_load_print_meta: BOS token        = 151643 '<|endoftext|>'
+llm_load_print_meta: EOS token        = 151645 '<|im_end|>'
+llm_load_print_meta: PAD token        = 151643 '<|endoftext|>'
+llm_load_print_meta: LF token         = 148848 'ÄĬ'
+llm_load_print_meta: EOT token        = 151645 '<|im_end|>'
+Available chat formats from metadata: chat_template.default
+Using gguf chat template: {% for message in messages %}{% if loop.first and messages[0]['role'] != 'system' %}{{ '<|im_start|>system
+You are a helpful assistant.<|im_end|>
+' }}{% endif %}{{'<|im_start|>' + message['role'] + '
+' + message['content'] + '<|im_end|>' + '
+'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant
+' }}{% endif %}
+Using chat eos_token: <|im_end|>
+Using chat bos_token: <|endoftext|>
+
+
+"""
---
+++ ARCEE-LITE-1.5B/201.Arcee-Lite-1.5B_autotest_CHAT.py
@@ -0,0 +1,206 @@
+# Chat with an intelligent assistant in your terminal 
+# original https://huggingface.co/arcee-ai/arcee-lite 
+# MODEL: https://huggingface.co/arcee-ai/arcee-lite-GGUF
+# models/arcee-lite-Q5_K_M.gguf
+import sys
+from time import sleep
+import warnings
+warnings.filterwarnings(action='ignore')
+import datetime
+import random
+import string
+import tiktoken
+import argparse
+
+#Add GPU argument in the parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--gpu", action="store_true")
+
+args = parser.parse_args()
+GPU = args.gpu
+if GPU:
+    ngpu_layers = 28
+    print(f'Selected GPU: offloading {ngpu_layers} layers...')
+else:
+     ngpu_layers = 0   
+     print('Loading Model on CPU only......')
+
+
+
+encoding = tiktoken.get_encoding("cl100k_base") #context_count = len(encoding.encode(yourtext))
+
+
+modelname = 'arcee-lite-Q5_K_M.gguf'
+
+def countTokens(text):
+    encoding = tiktoken.get_encoding("cl100k_base") #context_count = len(encoding.encode(yourtext))
+    numoftokens = len(encoding.encode(text))
+    return numoftokens
+
+def writehistory(filename,text):
+    # save the user/assistant pairs into a logfile located in the logs subdirectory
+    with open(f'logs/{filename}', 'a', encoding='utf-8') as f:
+        f.write(text)
+        f.write('\n')
+    f.close()
+
+def genRANstring(n):
+    """
+    n = int number of char to randomize
+    """
+    N = n
+    res = ''.join(random.choices(string.ascii_uppercase +
+                                string.digits, k=N))
+    return res
+
+# create THE LOG FILE 
+logfile = f'arcee-lite-1.5b_{genRANstring(5)}_CHAT_log.txt'
+logfilename = logfile
+#Write in the history the first 2 sessions
+writehistory(logfilename,f'{str(datetime.datetime.now())}\n\nYour own LocalGPT with 💻 {modelname}\n---\n🧠🫡: You are a helpful assistant.')    
+writehistory(logfilename,f'💻: How can I assist you today in writing?')
+
+
+STOPS = ['<|im_end|>']
+print("\033[95;3;6m")
+print("1. Waiting 10 seconds for the API to load...")
+from llama_cpp import Llama
+llm = Llama(
+            model_path=f'models/{modelname}',
+            n_gpu_layers=ngpu_layers,
+            temperature=1.1,
+            top_p = 0.5,
+            n_ctx=8192,
+            max_tokens=1500,
+            repeat_penalty=1.178,
+            stop=STOPS,
+            verbose=False,
+            )
+print(f"2. Model {modelname} loaded with LlamaCPP...")
+print("\033[0m")  #reset all
+history = []
+print("\033[92;1m")
+print(f'📝Logfile: {logfilename}')
+
+
+############################# START TURN BASED CHAT #################################
+print('Starting now Normal Chat turn based interface...')
+counter = 1
+while True:
+    # Reset history ALWAys
+    history = []        
+    userinput = ""
+    print("\033[1;30m")  #dark grey
+    print("Enter your text (end input with Ctrl+D on Unix or Ctrl+Z on Windows) - type quit! to exit the chatroom:")
+    print("\033[91;1m")  #red
+    lines = sys.stdin.readlines()
+    for line in lines:
+        userinput += line + "\n"
+    if "quit!" in lines[0].lower():
+        print("\033[0mBYE BYE!")
+        break
+    history.append({"role": "user", "content": userinput})
+    print("\033[92;1m")
+    # Preparing Generation history pair
+    new_message = {"role": "assistant", "content": ""}
+    # Starting generation loop
+    full_response = ""
+    fisrtround = 0
+    start = datetime.datetime.now()
+    print("💻 > ", end="", flush=True)
+    firsttoken = 0
+    for chunk in llm.create_chat_completion(
+        messages=history,
+        temperature=0.15,
+        repeat_penalty= 1.131,
+        stop=STOPS,
+        max_tokens=1500,
+        stream=True,):
+        try:
+            if chunk["choices"][0]["delta"]["content"]:
+                if firsttoken == 0:
+                    ttft = datetime.datetime.now() - start
+                    ttft_seconds = ttft.total_seconds()
+                    firsttoken = 1
+                    print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                    full_response += chunk["choices"][0]["delta"]["content"]  
+                else:
+                    print(chunk["choices"][0]["delta"]["content"], end="", flush=True)
+                    full_response += chunk["choices"][0]["delta"]["content"]                                                 
+        except:
+            pass        
+    new_message["content"] = full_response
+    history.append(new_message)  
+    counter += 1  
+    delta = datetime.datetime.now() - start
+    totalseconds = delta.total_seconds()
+    output = full_response
+    prompttokens = countTokens(userinput)
+    assistanttokens = countTokens(output)
+    totaltokens = prompttokens + assistanttokens
+    speed = totaltokens/totalseconds
+    genspeed = assistanttokens/totalseconds
+    print('')
+    print("\033[91;1m")
+    rating = input('Rate from 0 (BAD) to 5 (VERY GOOD) the quality of generation> ')
+    print("\033[92;1m")
+    stats = f'''---
+Prompt Tokens: {prompttokens}
+Output Tokens: {assistanttokens}
+TOTAL Tokens: {totaltokens}
+>>>⚡Time to first token: {ttft_seconds:.2f} seconds
+>>>⏱️Inference time: {delta}
+>>>🧮Inference speed: {speed:.3f}  t/s
+>>>🏃‍♂️Generation speed: {genspeed:.3f}  t/s
+📝Logfile: {logfilename}
+>>>💚User rating: {rating}
+
+'''
+    print(stats)
+    writehistory(logfilename,f'''👨‍💻: {userinput}
+💻 > {full_response}
+{stats}
+📝Logfile: {logfilename}''')
+
+##############################MODEL CARD###############################################
+"""
+
+Prompt format
+```
+<|im_start|>system
+{system_prompt}<|im_end|>
+<|im_start|>user
+{prompt}<|im_end|>
+<|im_start|>assistant
+```
+
+llm_load_print_meta: format           = GGUF V3 (latest)
+llm_load_print_meta: arch             = qwen2
+llm_load_print_meta: vocab type       = BPE
+llm_load_print_meta: n_vocab          = 151936
+llm_load_print_meta: n_merges         = 151387
+llm_load_print_meta: vocab_only       = 0
+llm_load_print_meta: n_ctx_train      = 32768
+llm_load_print_meta: n_embd           = 1536
+llm_load_print_meta: n_layer          = 28
+llm_load_print_meta: model ftype      = Q5_K - Medium
+llm_load_print_meta: model params     = 1.78 B
+llm_load_print_meta: model size       = 1.19 GiB (5.76 BPW)
+llm_load_print_meta: general.name     = Qwen2 1.5B
+llm_load_print_meta: BOS token        = 151643 '<|endoftext|>'
+llm_load_print_meta: EOS token        = 151645 '<|im_end|>'
+llm_load_print_meta: PAD token        = 151643 '<|endoftext|>'
+llm_load_print_meta: LF token         = 148848 'ÄĬ'
+llm_load_print_meta: EOT token        = 151645 '<|im_end|>'
+Available chat formats from metadata: chat_template.default
+Using gguf chat template: {% for message in messages %}{% if loop.first and messages[0]['role'] != 'system' %}{{ '<|im_start|>system
+You are a helpful assistant.<|im_end|>
+' }}{% endif %}{{'<|im_start|>' + message['role'] + '
+' + message['content'] + '<|im_end|>' + '
+'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant
+' }}{% endif %}
+Using chat eos_token: <|im_end|>
+Using chat bos_token: <|endoftext|>
+
+
+"""
